0de5d8236bfcfacd89dfe9b5f968cd4ac7e6e0c2
Justin Kenealy <justinmkenealy@gmail.com>
2023-04-22 15:59:54 +1000

    adds post, delete and puts methods to projects route

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,17 +1,71 @@
 const express = require('express');
 const { 
-    getProjectsByUser
+    getProjectsByUser,
+    createProject,
+    updateProject,
+    deleteProject
     } = require('../models/project');
 
 const router = express.Router();
 
+//get project (by user)
 router.get('/projects/:id', (req, res, next) => {
     const userID = Number(req.params.id);
-
     return getProjectsByUser(userID)
         .then((result) => {
             res.json(result)
         })
 })
+
+//create new project
+router.post('/projects/', (req, res, next) => {
+	const { user_id, collab, category, name, task_type } = req.body;
+
+    return createProject(user_id, collab, category, name, task_type)
+        .then((result) => {
+            res.status(201);
+            res.json({
+                user_id, 
+                collab, 
+                category,
+                name, 
+                task_type
+            });
+        }).catch((err) => {
+            console.log(err.message)
+            res.status(500).json({
+                message: 'Unexpected server error.'
+            })
+        });
+});
+
+//delete project
+router.delete('/projects/:id', (req, res, next) => {
+	const id = Number(req.params.id);
+	
+	return deleteProject(id)
+        .then(result => {
+            if (result.rowCount === 0) {
+                const customError = new Error('Cannot find challenge with id: ' + id)
+                customError.status = 404;
+                next(customError);
+            } else {
+                res.status(204).json({
+                    message: 'deleted'
+                })
+            }
+        })
+        .catch((err) => {
+            next(err);
+        });
+}); 
+
+//update project details
+router.put('/projects/:id', (req, res) => {
+	const id = Number(req.params.id);
+	const { collab, category, name, task_type } = req.body;
+    return updateProject(collab, category, name, task_type, id)
+    	.then((result) => res.sendStatus(result.rowCount === 0 ? 404 : 200))
+});
 
 module.exports = router;
--- models/project.js
+++ models/project.js
@@ -9,4 +9,23 @@ const getProjectsByUser = (user_id) => {
         .then(result => result.rows);
 }
 
-module.exports = {getProjectsByUser}
+const createProject = (user_id, collab, category, name, task_type) => {
+    const sql = 'INSERT INTO projects (user_id, collab, category, name, task_type) VALUES($1, $2, $3, $4, $5) RETURNING id;';
+    return db.query(sql, [user_id, collab, category, name, task_type])
+}   
+
+const deleteProject = (id) => {
+    return db.query('DELETE from projects WHERE id=$1', [id])
+}
+
+const updateProject = (collab, category, name, task_type, id) => {
+    const sql = `UPDATE projects SET collab = $1, category = $2, name = $3, task_type = $4 WHERE id = $5;`
+    return db.query(sql, [collab, category, name, task_type, id])
+}
+
+module.exports = {
+    getProjectsByUser,
+    createProject,
+    updateProject,
+    deleteProject
+}

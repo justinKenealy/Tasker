42f344cba5e42f05e20e7fd7be3b9a672be62678
Munsat Rukaya <munsatrukaya@192-168-1-103.tpgi.com.au>
2023-04-23 16:19:19 +1000

    adds bootstrap, edited render header to implement bootstrap, added conditions for displaying the app go login

--- client/js/components/renderHeader.js
+++ client/js/components/renderHeader.js
@@ -1,15 +1,56 @@
-const renderHeader = (user) => {
-    const header = document.getElementById('header-nav');
+const renderHeader = (userName) => {
+    const header = document.getElementById('header-nav')
 
     header.innerHTML = `
-    <img id="logo-img" src="./images/tskr-high-resolution-logo-color-on-transparent-background.png" alt="logo">
-        <p>Hello, __!!USER!!__<button class="logout-btn">Logout</button></p>
-        <ul>
-            <li role="button" class="nav-item" id="sortListByToday">Today</li>
-            <li role="button" class="nav-item" id="sortListByUpcoming">Upcoming</li>
-            <li role="button" class="nav-item" id="renderNotesPage">Notes</li>
-        </ul>
+    <nav class="navbar navbar-expand-lg" style="background-color: #bad7e9">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img id="logo-img" src="./images/tskr-high-resolution-logo-color-on-transparent-background.png" alt="logo"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse nav-underline justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item px-5">
+                        <a class="nav-link active" aria-current="page" href="#" id="sortListByToday">Today</a>
+                    </li>
+                    <li class="nav-item px-5">
+                        <a class="nav-link" href="#" id="sortListByUpcoming">Upcoming</a>
+                    </li>
+                    <li class="nav-item px-5">
+                        <a class="nav-link" href="#" id="renderNotesPage">Notes</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="nav-item dropdown px-5">
+                <a class="nav-link dropdown-toggle name-display" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    ${userName}
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item userDetail" href="#">View Details</a></li>
+                    <li><a class="dropdown-item logout-btn" href="#">Logout</a></li>
+                </ul>
+            </div>
+        </div>
+      </nav>    
     `
+    const logoutBtn = document.querySelector('.logout-btn')
+    logoutBtn.addEventListener('click', () => {
+        axios
+            .delete('/api/session')
+            .then((res) => (window.location = '/entry.html'))
+    })
+
+    // const userDetail = document.querySelector('.change-pass')
+    // userDetail.addEventListener('click', () => {
+        
+    // })
 }
 
 export default renderHeader
+
+/* <p class="name-display"><button class="logout-btn">Logout</button></p>
+<ul>
+    <li role="button" class="nav-item" id="sortListByToday">Today</li>
+    <li role="button" class="nav-item" id="sortListByUpcoming">Upcoming</li>
+    <li role="button" class="nav-item" id="renderNotesPage">Notes</li>
+</ul> */
--- client/js/init.js
+++ client/js/init.js
@@ -1,4 +1,13 @@
 import renderHeader from "./components/renderHeader.js";
 import renderLeftPane from "./components/renderLeftPane.js";
-renderHeader()
-renderLeftPane()
+
+axios.get('/api/session')
+.then(({data}) => {
+    console.log(data)
+    if('user' in data){
+        renderHeader(data.user.name)
+        renderLeftPane(data.user.id)
+    }else{
+        window.location = '/entry.html'
+    }
+}).catch(err => console.error(err))

6ec2df85c559e52e61327371333f7bcacea56672
Justin Kenealy <justinmkenealy@gmail.com>
2023-04-25 12:07:04 +1000

    new project form displays checkboxes to add collaborators using users' friends array

--- client/js/components/renderLeftPane.js
+++ client/js/components/renderLeftPane.js
@@ -1,4 +1,5 @@
 import renderTasks from "./renderTasks.js"
+import { renderNewProjectForm, handleFormSubmit } from "./renderNewProjectForms.js"
 
 const renderLeftPane = async (user) => {
     const personalProjectsListUl = document.getElementById('personalProjectsListUl')
@@ -17,7 +18,7 @@ const renderLeftPane = async (user) => {
                 newLi.innerHTML = project.name
                 if (project.task_type === 'group'){
                     newLi.classList.add('groupProject')
-                    newLi.innerHTML += ` - &#1011${project.collab.length + 1}`
+                    // newLi.innerHTML += ` - &#1011${project.collab.length + 1}`
                 }
                 if (project.category === 'work'){
                     workProjectsListUl.appendChild(newLi)
@@ -48,78 +49,6 @@ const renderLeftPane = async (user) => {
         addStudyProjectBtn.addEventListener('click', function(){
             renderNewProjectForm('study', user)
         })
-}
-
-const renderNewProjectForm = (projectCategory, user) => {    
-    const display = document.createElement("div");
-    display.className = "display";
-    document.body.prepend(display);
-
-    const cancelIcon = document.createElement('i')
-    cancelIcon.className = 'fa-solid fa-xmark cancel-icon'
-
-    const newProjectForm = document.createElement("div");
-    newProjectForm.innerHTML = `
-    <form id="create-project-form">
-        <p>
-            <label for="name">Project Name</label></br>
-            <input type="text" name="name"></input>
-        </p>
-        <p>
-            <label for="projectType">Type</label><br>
-            <input type="radio" id="projectTypeIndividual" name="projectType" value="single">
-            <label for="individual">Individual</label><br>
-            <input type="radio" id="projectTypeGroup" name="projectType" value="group">
-            <label for="group">Group</label><br>
-        </p>
-        <p id="collaboratorsFormField">
-            <label for="collaborators">Collaborators</label>
-            <select multiple name="collaborators">
-                <option>Person A</option>
-                <option>Person B</option>
-                <option>Person C</option>
-            </select>
-        </p>
-        <input type="hidden" name="userId" value=${user.id}></input>
-        <input type="hidden" name="category" value=${projectCategory}></input>
-        <button class="btn-style" type="submit">Create</button>
-    </form>
-    `;
-
-    display.appendChild(cancelIcon)
-    display.appendChild(newProjectForm)
-
-    cancelIcon.addEventListener("click", () => display.remove())
-    document.getElementById("create-project-form").addEventListener("submit", function(event){
-        handleFormSubmit(event, user)})
-    document.getElementById("projectTypeGroup").addEventListener('click', function(){
-        document.getElementById('collaboratorsFormField').style.display = 'block'
-    })
-    document.getElementById("projectTypeIndividual").addEventListener('click', function(){
-        document.getElementById('collaboratorsFormField').style.display = 'none'
-    })
-}
-
-const handleFormSubmit = (event, user) => {
-    event.preventDefault()
-    document.querySelector('.display').remove()
-    const formData = new FormData(event.target);
-    
-    const body = {
-        user_id: Number(formData.get('userId')),
-        collab: formData.get('collaborators'),
-        category: formData.get('category'),
-        name: formData.get('name'),
-        task_type: formData.get('projectType')
-    };
-
-    return axios.post('/api/projects', body)
-    .then(res => {
-        renderLeftPane(user);
-    })
-    .catch(err => {
-        console.error(err)
-    })
 }
 
 export default renderLeftPane
---
+++ client/js/components/renderNewProjectForms.js
@@ -0,0 +1,79 @@
+import renderLeftPane from "./renderLeftPane.js";
+
+const renderNewProjectForm = (projectCategory, user) => {    
+    const display = document.createElement("div");
+    display.className = "display";
+    document.body.prepend(display);
+
+    const cancelIcon = document.createElement('i')
+    cancelIcon.className = 'fa-solid fa-xmark cancel-icon'
+
+    const newProjectForm = document.createElement("div");
+    newProjectForm.innerHTML = `
+    <form id="create-project-form">
+        <p>
+            <label for="name">Project Name</label></br>
+            <input type="text" name="name"></input>
+        </p>
+        <p>
+            <label for="projectType">Type</label><br>
+            <input type="radio" id="projectTypeIndividual" name="projectType" value="single">
+            <label for="individual">Individual</label><br>
+            <input type="radio" id="projectTypeGroup" name="projectType" value="group">
+            <label for="group">Group</label><br>
+        </p>
+        <p id="collaboratorsFormField" style="display:none;">
+            <label for="collaborators">Collaborators</label>
+        </p>
+        <input type="hidden" name="userId" value=${user.id}></input>
+        <input type="hidden" name="category" value=${projectCategory}></input>
+        <button class="btn-style" type="submit">Create</button>
+    </form>
+    `;
+    display.appendChild(cancelIcon)
+    display.appendChild(newProjectForm)
+    const collaboratorsFormField = document.getElementById('collaboratorsFormField')
+
+    for (let friend of user.friends_array){
+        const newFriendCheckBox = document.createElement('div')
+        newFriendCheckBox.innerHTML = `
+        <input type="checkbox" id=${friend} name="collaborators" value=${friend}>
+        <label for=${friend}>${friend}</label>
+        `
+        collaboratorsFormField.appendChild(newFriendCheckBox)
+    }
+
+    cancelIcon.addEventListener("click", () => display.remove())
+    document.getElementById("create-project-form").addEventListener("submit", function(event){
+        handleFormSubmit(event, user)})
+    document.getElementById("projectTypeGroup").addEventListener('click', function(){
+        collaboratorsFormField.style.display = 'block'
+    })
+    document.getElementById("projectTypeIndividual").addEventListener('click', function(){
+        collaboratorsFormField.style.display = 'none'
+    })
+}
+
+const handleFormSubmit = (event, user) => {
+    event.preventDefault()
+    document.querySelector('.display').remove()
+    const formData = new FormData(event.target);
+
+    const body = {
+        user_id: Number(formData.get('userId')),
+        collab: formData.getAll('collaborators'),
+        category: formData.get('category'),
+        name: formData.get('name'),
+        task_type: formData.get('projectType')
+    };
+
+    return axios.post('/api/projects', body)
+    .then(res => {
+        renderLeftPane(user);
+    })
+    .catch(err => {
+        console.error(err)
+    })
+}
+
+export { renderNewProjectForm, handleFormSubmit }

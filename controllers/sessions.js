430f465326131a1019fc3c587784253b3373edbc
Munsat Rukaya <munsatrukaya@192-168-1-103.tpgi.com.au>
2023-04-26 14:27:23 +1000

    added recaptcha verification to login and signup

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,6 +1,7 @@
 //Contains all API route definations for sessions
 
 const express = require('express')
+const axios = require('axios')
 const bcrypt = require('bcrypt')
 const { getUserByEmail } = require('../models/user')
 const router = express.Router()
@@ -9,50 +10,66 @@ const comparePassword = (password, password_hash) => {
     const result = bcrypt.compareSync(password, password_hash)
     return result
 }
+const recaptchaSecretKey = process.env.RECAPTCHA_SECRET_KEY
 
-router.post('/session', async (req,res,next) => {
-    try{
-        const {email, password} = Object.entries(req.body).reduce((obj, [key, value])=>{
-            obj[key] = value.trim()
-            return obj
-        }, {})
-    
-        if (!email || !password) {
-            const customError = new Error("The email or password is missing.");
-            customError.status = 400;
-            return next(customError);
-        }
-    
-        const user = await getUserByEmail(email)
-        if(user){
-            if(comparePassword(password, user.password_hash)){
-                delete user.password_hash
-                req.session.user = user
-                return res.status(200).json({
-                    message: 'Logged in successfully.'
-                })
+router.post('/session', async (req, res, next) => {
+    try {
+        //Verify captcha
+        const captchaResponse = req.body['g-recaptcha-response']
+        const captchaVerification = await axios.post(
+            `https://www.google.com/recaptcha/api/siteverify?secret=${recaptchaSecretKey}&response=${captchaResponse}`
+        )
+        if (captchaVerification.data.success === true) {
+            const { email, password } = Object.entries(req.body).reduce(
+                (obj, [key, value]) => {
+                    obj[key] = value.trim()
+                    return obj
+                },
+                {}
+            )
+
+            if (!email || !password) {
+                const customError = new Error(
+                    'The email or password is missing.'
+                )
+                customError.status = 400
+                return next(customError)
             }
-        } 
-        const customError = new Error("Sorry, invalid email or password.")
-        customError.status = 400
-        return next(customError)
-    }catch (err){
+
+            const user = await getUserByEmail(email)
+            if (user) {
+                if (comparePassword(password, user.password_hash)) {
+                    delete user.password_hash
+                    req.session.user = user
+                    return res.status(200).json({
+                        message: 'Logged in successfully.',
+                    })
+                }
+            }
+            const customError = new Error('Sorry, invalid email or password.')
+            customError.status = 400
+            return next(customError)
+        } else {
+            const customError = new Error(
+                'Please complete the recaptcha challenge to proceed.'
+            )
+            customError.status = 409
+            return next(customError)
+        }
+    } catch (err) {
         next(err)
     }
 })
 
-router.get('/session',(req,res,next)=>{
-    
+router.get('/session', (req, res, next) => {
     const { user } = req.session
-    return res.status(200).json({user})
+    return res.status(200).json({ user })
 })
 
-
-router.delete('/session', async (req,res,next) => {
-    
-    req.session.destroy(()=>{
+router.delete('/session', async (req, res, next) => {
+    req.session.destroy(() => {
         return res.status(200).json({
-            message:'Logged out'
+            message: 'Logged out',
         })
     })
 })

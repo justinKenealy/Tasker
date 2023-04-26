8deff40a447456f6f9e866f33cf7be1444bdd4c5
Justin Kenealy <justinmkenealy@gmail.com>
2023-04-26 13:35:01 +1000

    updates styling and functionality of left pane

--- client/js/components/renderLeftPane.js
+++ client/js/components/renderLeftPane.js
@@ -3,9 +3,9 @@ import { renderNewProjectForm } from "./renderNewProjectForm.js"
 import { renderEditDeleteProjectForm } from "./renderEditDeleteProjectForm.js"
 
 const renderLeftPane = async (user) => {
-    const personalProjectsListUl = document.getElementById('personalProjectsListUl')
-    const workProjectsListUl = document.getElementById('workProjectsListUl')
-    const studyProjectsListUl = document.getElementById('studyProjectsListUl')
+    const personalProjectsListUl = document.getElementById('personal-projects-list-ul')
+    const workProjectsListUl = document.getElementById('work-projects-list-ul')
+    const studyProjectsListUl = document.getElementById('study-projects-list-ul')
     
     personalProjectsListUl.innerHTML=''
     workProjectsListUl.innerHTML=''
@@ -15,24 +15,35 @@ const renderLeftPane = async (user) => {
         .then((response) => {
         const { data } = response
             for (let project of data){
-                const newLi = document.createElement('li')
-                const editButton = document.createElement('button')
-                editButton.innerHTML = 'Edit'
-                newLi.innerText = project.name
-                newLi.appendChild(editButton)
+                const newDiv = document.createElement('div')
+                newDiv.classList += 'project-list-item'
+                const projectName = document.createElement('p')
+                projectName.classList += 'project-name'
+                projectName.innerText = project.name
+                newDiv.appendChild(projectName)
+                const editButton = document.createElement('i')
+                editButton.classList = 'fa-solid fa-ellipsis project-edit-button'
 
                 if (project.task_type === 'group'){
-                    newLi.classList.add('groupProject')
-                    // newLi.innerHTML += ` - &#1011${project.collab.length + 1}`
-                }
-                if (project.category === 'work'){
-                    workProjectsListUl.appendChild(newLi)
-                } else if (project.category === 'personal'){
-                    personalProjectsListUl.appendChild(newLi)
+                    newDiv.classList.add('group-project')
+                    const groupIcon = document.createElement('i')
+                    newDiv.appendChild(groupIcon)
+                    groupIcon.className = 'fa-solid fa-user-group'
                 } else {
-                    studyProjectsListUl.appendChild(newLi)
+                    const individualIcon = document.createElement('i')
+                    newDiv.appendChild(individualIcon)
+                    individualIcon.className = 'fa-solid fa-user'
                 }
-                newLi.addEventListener('click', async function() {
+                
+                if (project.category === 'work'){
+                    workProjectsListUl.appendChild(newDiv)
+                } else if (project.category === 'personal'){
+                    personalProjectsListUl.appendChild(newDiv)
+                } else {
+                    studyProjectsListUl.appendChild(newDiv)
+                }
+                newDiv.appendChild(editButton)
+                projectName.addEventListener('click', async function() {
                     const tasks = await axios.get('/api/tasks/project/' + project.id) 
                     renderTasks(tasks.data, project.name)
                 })
@@ -43,9 +54,9 @@ const renderLeftPane = async (user) => {
             }
         })
         
-        const addPersonalProjectBtn = document.getElementById('addPersonalProjectBtn')
-        const addWorkProjectBtn = document.getElementById('addWorkProjectBtn')
-        const addStudyProjectBtn = document.getElementById('addStudyProjectBtn')
+        const addPersonalProjectBtn = document.getElementById('add-personal-project-btn')
+        const addWorkProjectBtn = document.getElementById('add-work-project-btn')
+        const addStudyProjectBtn = document.getElementById('add-study-project-btn')
 
         addPersonalProjectBtn.addEventListener('click', function(){
             renderNewProjectForm('personal', user)

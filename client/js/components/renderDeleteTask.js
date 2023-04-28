bd8b016fac35afbe73f9698241c0d74c62e8c972
Justin Kenealy <justinmkenealy@gmail.com>
2023-04-29 02:08:11 +1000

    fixed delete task function so tasks disappear

--- client/js/components/renderDeleteTask.js
+++ client/js/components/renderDeleteTask.js
@@ -1,20 +1,29 @@
 import renderTasks from "./renderTasks.js"
 const renderDeleteTask = async (taskId, projectTitle, projectID) => {
-    // const confirmed = confirm("Are you sure you want to delete this task?")
+    const confirmed = confirm("Are you sure you want to delete this task?")
     console.log(taskId)
+    console.log(projectTitle)
     console.log(projectID)
 
+    function deleteTask(){
     return axios.delete(`/api/tasks/${taskId}`)
         .then(async res => {
-            document.querySelector('#main-content').innerHTML = ''
-            const response = await axios.get(`/api/tasks/project/${projectID}`)
             console.log(res)
-            renderTasks(response.data, projectTitle, projectID)
-
         })
         .catch(err => {
             console.log(err)
         })
+    }
+    
+    async function renderNew(){
+        document.querySelector('#main-content').innerHTML = ''
+        const response = await axios.get(`/api/tasks/project/${projectID}`)
+        renderTasks(response.data, projectTitle, projectID)
+    }
+
+    if (confirmed){
+        deleteTask().then(renderNew())
+    }
 }
 
 export default renderDeleteTask
